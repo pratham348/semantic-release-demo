d692e99170342839f92549b083100682b4a200ab
Pratham Chauhan <prathamchauhan348@gmail.com>
2024-01-27 18:34:08 +0530

    feat(app): updated the script to get the proper version

--- update-version.js
+++ update-version.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
-const packageJson = require('./package.json');
-const version = packageJson.version;
-console.log("version", version);
-const data = {
-  version: version,
-  // Add any other properties you want in version.json
-};
 
-fs.writeFileSync('version.json', JSON.stringify(data, null, 2));
+try {
+  const version = process.env.npm_package_version || 'fallback version';
 
-console.log(`version.json updated with version: ${version}`);
+  const data = {
+    version: version,
+  };
+
+  fs.writeFileSync('version.json', JSON.stringify(data, null, 2));
+
+  console.log('version.json updated with version:', version);
+} catch (error) {
+  console.error('Error updating version.json:', error.message);
+}
