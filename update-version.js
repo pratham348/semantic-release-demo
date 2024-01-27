3799d6315a4c24c403f793099fbd326c4d338c06
Pratham Chauhan <prathamchauhan348@gmail.com>
2024-01-27 19:20:06 +0530

    fix(scrip): fix the script

--- update-version.js
+++ update-version.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 const versionJsonPath = path.join(process.cwd(), 'version.json');
 console.log('versionJsonPath',versionJsonPath);
+
 const incrementVersion = newVersion => {
   const versionJson = JSON.parse(fs.readFileSync(versionJsonPath, 'utf8'));
   versionJson.version = newVersion;
+  
   fs.writeFileSync(versionJsonPath, JSON.stringify(versionJson, null, 2) + '\n', 'utf8');
   console.log(`Version forcefully updated to: ${newVersion}`);
 };
