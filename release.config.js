315b552f6124ecc05316eb71d738c1905eceb68a
Pratham Chauhan <prathamchauhan348@gmail.com>
2024-01-27 17:45:25 +0530

    feat(versioning): added version.json file and added step to wrtie in that file with new release

--- release.config.js
+++ release.config.js
@@ -6,6 +6,23 @@ module.exports = {
       '@semantic-release/changelog',
       '@semantic-release/npm',
       '@semantic-release/git',
+      '@semantic-release/github',
+      {
+        // Custom plugin for updating version.json
+        async prepare(pluginConfig, context) {
+          const { nextRelease } = context;
+          const versionFilePath = './version.json';
+  console.log('versionFilePath',versionFilePath)
+          const versionData = {
+            version: nextRelease.version,
+          };
+          console.log('versionData',versionData)
+  
+          await jsonfile.writeFile(versionFilePath, versionData, { spaces: 2 });
+  
+          return {};
+        },
+      },
     ],
   };
   
